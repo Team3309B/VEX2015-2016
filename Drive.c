2528d1fe56e41da23e7d5238092ca10d0a987043
Matthew Krager <metoofarmer@yahoo.com>
2015-05-18 22:09:00 -0700

    messed with defines

--- Drive.c
+++ Drive.c
@@ -1,5 +1,27 @@
 
 
+void setLeftDrive(int x) {
+	motor[LeftFrontDrive] = x;
+	motor[LeftBackDrive] = x;
+}
+
+void setRightDrive(int x) {
+	motor[RightFrontDrive] = x;
+	motor[RightBackDrive] = x;
+}
+
+void runDriveAt(int x) {
+	setLeftDrive(x);
+	setRigtDrive(x);
+}
+
+void stopDrive() {
+	runDriveAt(0);
+}
+
 task driveTask() {
 
+	while(true) {
+
+	}
 }
--- LCD.c
+++ LCD.c
@@ -1,7 +1,5 @@
 
-task main()
-{
 
-
+task LCD() {
 
 }
